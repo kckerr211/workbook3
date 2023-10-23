82c173526848f384c693d7ce056dd3446343572c
Khalil Kerr <kckerr211@gmail.com>
2023-10-23 14:56:03 -0400

    functions

---
+++ Functions/simple_functions.js
@@ -0,0 +1,12 @@
+
+function favoriteThings() {
+  console.log("Khalil Kerr");
+  console.log("Black Panther");
+  console.log("NoCap");
+}
+function whyImHere() {
+  console.log("I'm here to get a good paying job");
+}
+function favoritePlace() {
+  console.log("My boy's crib is my favorite place to visit because thats where we all meet");
+}
